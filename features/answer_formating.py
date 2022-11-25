aa61f780c1cbfa3945f0908ca1f29787c1bd6913
ivan <ivansyn2013@yandex.ru>
2022-11-25 18:58:51 +0300

    все работает

--- features/answer_formating.py
+++ features/answer_formating.py
@@ -4,13 +4,14 @@ def one_srt_answer(response: dict, inx: int):
              #f'{r["Название продукта"][0]}\n' \
              #f'{r["high low medium"][0]}\n\n' \
              #f'Безопасная доза' \
-    answer = f'{r["Доза"][inx]} {r["Порция"][inx]}\n' \
+    answer = f'{r["Доза"][inx]}\n ({r["Порция"][inx]})\n' \
              f'Фруктоза {(r["Отображение"][r["Фруктоза"][inx]-1])[0]} ' \
              f'Лактоза  {(r["Отображение"][(r["Лактоза"][inx])-1])[0]} \n' \
              f'Маннитол {(r["Отображение"][(r["Маннитол"][inx])-1])[0]} ' \
              f'Сорбитол {(r["Отображение"][(r["Сорбитол"][inx])-1])[0]}\n' \
              f'ГОС      {(r["Отображение"][(r["ГОС"][inx])-1])[0]} ' \
-             f'Фруктаны {(r["Отображение"][(r["Фруктаны"][inx])-1])[0]}\n'
+             f'Фруктаны {(r["Отображение"][(r["Фруктаны"][inx])-1])[0]}\n' \
+
     return answer
 
 
@@ -27,7 +28,9 @@ def get_answer_str(response: dict):
         return None
     elif 4 > len(r['Доза']) > 0 :
         for inx in range(len(list(r.values())[0])):
-            answer = f'{answer}{dose_str[inx]} {one_srt_answer(r,inx)}\n'
+            answer = f'{answer}{dose_str[inx]} {one_srt_answer(r,inx)}\n '
+            if r["Примечание"][0] is not None:
+                answer = answer + f'{r["Примечание"][0]}'
 
         return answer
     else:
