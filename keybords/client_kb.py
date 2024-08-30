4c6a435768a6dd18ac3e563c7bcd28e5861df7c7
ivan_note <ivansyn2013@yandex.ru>
2024-08-30 10:26:44 +0300

    - edit output Примечания - edit start keyboard

--- keybords/client_kb.py
+++ keybords/client_kb.py
@@ -7,6 +7,8 @@ b2 = types.KeyboardButton(text='Поиск')
 b3 = types.KeyboardButton(text='Категории продуктов')
 b4 = types.KeyboardButton(text='Список продуктов')
 b5 = types.KeyboardButton(text='Поддержать проект')
+b6 = types.KeyboardButton(text='Консультации')
+b7 = types.KeyboardButton(text='Обратная связь')
 
 
 search_b1 = types.KeyboardButton(text='Искать')
@@ -19,11 +21,10 @@ search_b2 = types.KeyboardButton(text='Выйти из поиска')
 #one_time_keyboard=True для одноразового показа, но клавиатуру можно вернуть для
 # полного удаления нужно в хендлере прописать reply_markup=ReplyKeyboardRemove()
 kb_client = types.ReplyKeyboardMarkup(
-    keyboard=[[b1],
-              [b2],
-              [b3],
-              [b4],
-              [b5]
+    keyboard=[[b1, b2],
+              [b3, b4],
+              [b5],
+              [b6, b7],
               ],
     resize_keyboard=True,
     input_field_placeholder="Выберите способ подачи",
