f923befae9bc813cf02b15c513d4566dbbf42efa
ivan_note <ivansyn2013@yandex.ru>
2024-10-20 17:09:45 +0300

    - change search style - add new keyboard for it

--- keybords/__init__.py
+++ keybords/__init__.py
@@ -1,4 +1,4 @@
-from keybords.client_kb import kb_client, kb_search
+from keybords.client_kb import kb_client, kb_search, get_inline_search_kb
 from keybords.other_kb import kb_answer_and_qusetion
 from keybords.admin_kb import admin_kb_check, ADMIN_KB_SENDMESSAGE
 from keybords.donate_kb import DONATE_KB
--- keybords/donate_kb.py
+++ keybords/donate_kb.py
@@ -1,14 +1,13 @@
 from aiogram import types
 import os
 
-
 button_data = {
     '100 руб.': os.getenv('PAY_LINK_100'),
     '200 руб.': os.getenv('PAY_LINK_200'),
     '300 руб.': os.getenv('PAY_LINK_300'),
     '400 руб.': os.getenv('PAY_LINK_400'),
     '500 руб.': os.getenv('PAY_LINK_500'),
-    '600 руб.': os.getenv('PAY_LINK_600'),
+    '1000 руб.': os.getenv('PAY_LINK_1000'),
     '5000 руб.': os.getenv('PAY_LINK_5000'),
 }
 
