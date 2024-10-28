70e99952e443a446f51efa8ebd17d05af71bf6b3
ivan_note <ivansyn2013@yandex.ru>
2024-10-28 21:27:44 +0300

    - change settings for webhook - change start serach message

--- Deploy/scripts/req_webhook.py
+++ Deploy/scripts/req_webhook.py
@@ -46,7 +46,7 @@ class Webhooks_setttings():
                             json={'url':f'{self.WEBHOOK_URL}{self.WEBHOOK_PATH}:{self.WEBHOOK_PORT}',
                                 'certificate':f'{file}'},
                             )
-        print(f'Webhook registering on this adress: {self.WEBHOOK_URL}{self.WEBHOOK_PATH}:{self.WEBHOOK_PORT}')
+        print(f'Webhook registering on this adress: {self.WEBHOOK_URL}:{self.WEBHOOK_PORT}{self.WEBHOOK_PATH}')
         self._say_answer(req)
 
     def get_webhook_info(self):
