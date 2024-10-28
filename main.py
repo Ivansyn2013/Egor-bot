1912f99b5b8bd2cc64748b04c6fb2d6a48d758da
ivan_note <ivansyn2013@yandex.ru>
2024-10-28 21:02:56 +0300

    - change settings for webhook - change start serach message

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ async def on_startup():
     # dp.outer_middleware.setup(CheckUserMiddleware())
     if DEBUG == "False":
         logger.info('Webhook mode start set.webhook')
+        logger.info(f'Set webhook: {WEBHOOK_URL}{WEBHOOK_PATH}')
         await bot.set_webhook(f"{WEBHOOK_URL}{WEBHOOK_PATH}",
                                   secret_token=WEBHOOK_SECRET)
-        logger.info(f'Set webhook: {WEBHOOK_URL}{WEBHOOK_PATH}')
 
 
 
